00f68969377a2eae4e158bc80f7e70f1b3d3140a
adityashrivastavaaaaaaa-lang <arkashrios@gmail.com>
2026-05-24 16:42:53 +0530

    Remove Supabase preview migration

--- supabase/migrations/20260524000000_add_student_aadhar_fields.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-BEGIN
-  IF to_regclass('public.students') IS NOT NULL THEN
-    ALTER TABLE public.students
-      ADD COLUMN IF NOT EXISTS aadhar_name VARCHAR(255),
-      ADD COLUMN IF NOT EXISTS aadhar_number VARCHAR(12);
-  END IF;
-END $$;
